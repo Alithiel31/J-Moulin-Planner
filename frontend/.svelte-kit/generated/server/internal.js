f5cad6927b86c92e771a7ff7c11ff430c46009b8
Alithiel31 <harmo.man.01@gmail.com>
2026-07-02 15:11:17 -0400

    fix eslint-frontend

--- frontend/.svelte-kit/generated/server/internal.js
+++ frontend/.svelte-kit/generated/server/internal.js
@@ -1,15 +1,21 @@
-
 import root from '../root.js';
 import { set_building, set_prerendering } from '$app/env/internal';
 import { set_assets } from '$app/paths/internal/server';
 import { set_manifest, set_read_implementation } from '__sveltekit/server';
-import { set_private_env, set_public_env } from '../../../node_modules/@sveltejs/kit/src/runtime/shared-server.js';
+import {
+	set_private_env,
+	set_public_env,
+} from '../../../node_modules/@sveltejs/kit/src/runtime/shared-server.js';
 import error from '../shared/error-template.js';
 
 export const options = {
 	app_template_contains_nonce: false,
 	async: false,
-	csp: {"mode":"auto","directives":{"upgrade-insecure-requests":false,"block-all-mixed-content":false},"reportOnly":{"upgrade-insecure-requests":false,"block-all-mixed-content":false}},
+	csp: {
+		mode: 'auto',
+		directives: { 'upgrade-insecure-requests': false, 'block-all-mixed-content': false },
+		reportOnly: { 'upgrade-insecure-requests': false, 'block-all-mixed-content': false },
+	},
 	csrf_check_origin: true,
 	csrf_trusted_origins: [],
 	embedded: false,
@@ -17,16 +23,23 @@ export const options = {
 	env_private_prefix: '',
 	hash_routing: false,
 	hooks: null, // added lazily, via `get_hooks`
-	preload_strategy: "modulepreload",
+	preload_strategy: 'modulepreload',
 	root,
 	service_worker: false,
 	service_worker_options: undefined,
 	server_error_boundaries: false,
 	templates: {
-		app: ({ head, body, assets, nonce, env }) => "<!doctype html>\n<html lang=\"en\">\n\t<head>\n\t\t<meta charset=\"utf-8\" />\n\t\t<link rel=\"icon\" href=\"" + assets + "/favicon.png\" />\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n\t\t<title>J-Moulin Planner</title>\n\t\t" + head + "\n\t</head>\n\t<body data-sveltekit-preload-data=\"hover\">\n\t\t<div style=\"display: contents\">" + body + "</div>\n\t</body>\n</html>\n",
-		error
+		app: ({ head, body, assets, nonce, env }) =>
+			'<!doctype html>\n<html lang="en">\n\t<head>\n\t\t<meta charset="utf-8" />\n\t\t<link rel="icon" href="' +
+			assets +
+			'/favicon.png" />\n\t\t<meta name="viewport" content="width=device-width, initial-scale=1" />\n\t\t<title>J-Moulin Planner</title>\n\t\t' +
+			head +
+			'\n\t</head>\n\t<body data-sveltekit-preload-data="hover">\n\t\t<div style="display: contents">' +
+			body +
+			'</div>\n\t</body>\n</html>\n',
+		error,
 	},
-	version_hash: "no7l88"
+	version_hash: 'no7l88',
 };
 
 export async function get_hooks() {
@@ -35,11 +48,9 @@ export async function get_hooks() {
 	let handleError;
 	let handleValidationError;
 	let init;
-	
 
 	let reroute;
 	let transport;
-	
 
 	return {
 		handle,
@@ -48,8 +59,16 @@ export async function get_hooks() {
 		handleValidationError,
 		init,
 		reroute,
-		transport
+		transport,
 	};
 }
 
-export { set_assets, set_building, set_manifest, set_prerendering, set_private_env, set_public_env, set_read_implementation };
+export {
+	set_assets,
+	set_building,
+	set_manifest,
+	set_prerendering,
+	set_private_env,
+	set_public_env,
+	set_read_implementation,
+};
